cd478e752db367b999f4d229dafd4255d6056b25
KTachibanaM <whj19931115@gmail.com>
2025-03-02 19:52:30 +0000

    add stuff to devcontainer

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y redis libsass-dev
+sudo apt install -y python3-pip redis libsass-dev
+
+pip3 install -r ./scripts/requirements.txt
 
 curl https://nim-lang.org/choosenim/init.sh -sSf | sh
 echo 'export PATH=/home/vscode/.nimble/bin:$PATH' >> ~/.bashrc
